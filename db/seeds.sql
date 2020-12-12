cddd37e8a2ea254ef4f3e700eee8ab1aabd678d9
Dimitar Micevski <dmicevski@hotmail.com>
2020-12-11 22:34:58 -0500

    updated schema and seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Hamburger');
 INSERT INTO burgers (name) VALUES ('Cheese Burger');
-INSERT INTO burgers (name, Devour) VALUES ('Bacon Cheese Burger', true);
-INSERT INTO burgers (name, Devour) VALUES ('Guacamole Burger', true);
-INSERT INTO burgers (name, Devour) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, Devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (name, Devoured) VALUES ('Guacamole Burger', true);
+INSERT INTO burgers (name, Devoured) VALUES ('Veggie Burger', true);
 INSERT INTO burgers (name) VALUES ('Five Cheese Burger');
